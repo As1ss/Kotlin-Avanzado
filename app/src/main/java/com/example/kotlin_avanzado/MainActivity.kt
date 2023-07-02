c799984ee646ce5399c313134406ec3687277533
As1ss <alexis.lopbri@educa.jcyl.es>
2023-07-02 21:31:09 +0200

    Ejercicio práctico realizado para asentar los conceptos de las expresiones Lambda

--- app/src/main/java/com/example/kotlin_avanzado/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_avanzado/MainActivity.kt
@@ -40,10 +40,125 @@ class MainActivity : AppCompatActivity() {
 
         println("Suma de los elementos $num1 + $num2 = ${calculadora(num1, num2, ::sumar)}")
         println("Resta de los elementos $num1 - $num2 = ${calculadora(num1, num2, ::restar)}")
-        println("Multiplicación de los elementos $num1 * $num2 = ${calculadora(num1, num2, ::multiplicar)}")
+        println(
+            "Multiplicación de los elementos $num1 * $num2 = ${
+                calculadora(
+                    num1,
+                    num2,
+                    ::multiplicar
+                )
+            }"
+        )
         println("División de los elementos $num1 / $num2 = ${calculadora(num1, num2, ::dividir)}")
 
+
+        //Creamos un objeto de tipo Person para comprobar las funciones de orden superior en objetos con los métodos creados de
+        // comprobar altura para ser policiía
+
+        var persona1: Person = Person("Antonio", "B123451", 1.60f)
+        println("Puede ser policía ${persona1.name} en Colombia? ${persona1.checkPolice(::alturaColombia)}")
+        println("Puede ser policía ${persona1.name} en España? ${persona1.checkPolice(::alturaSpain)}")
+
+
+        //EXPRESIONES LAMBDA
+
+        //Creamos una variable que guarde una expresión o función Lambda
+        var funcionLambda = { x: Int, y: Int -> x + y }
+
+        //Operamos sobre el ejemplo anterior de suma de dos ints pero pasando como parámetro esta variable
+        println("Suma de los elementos $num1 + $num2 = ${calculadora(num1, num2, funcionLambda)}")
+
+        //Estas funciones/expresiones nos permiten actualizar el bloque de código para que se adapten según nuestras necesidades, por ejemplo
+        // esta variable ha pasado de sumar dos valores enteros con un valor predefinido a restear dos valores que le acabamos de pasar ahora
+        funcionLambda = { x: Int, y: Int -> x - y }
+        println("Resta de los elementos 35 - 22 = ${calculadora(35, 22, funcionLambda)}")
+
+        //Ahora podemos realizar operaciones sin necesidad de tener estas expresiones almacenadas en ninguna variable
+        println(
+            "Multiplicación de los elementos $num1 * $num2 = ${
+                calculadora(
+                    num1,
+                    num2
+                ) { x: Int, y: Int -> x * y }
+            }"
+        )
+        println(
+            "División de los elementos $num1 / $num2 = ${
+                calculadora(
+                    num1,
+                    num2
+                ) { num1, num2 -> num1 / num2 }
+            }"
+        )
+
+        //Podemos extender estas expresiones para operaciones mas complejas, con variables
+        // anonimas, grupos de instrucciones y se pueden poner fuera del parentesis entre dos llaves
+        println(
+            "La potencia de  $num1 elevado a $num2 = ${
+                calculadora(num1, num2) { x, y ->
+                    var valor = 1
+                    for (i in 1..y) valor *= x
+
+                    valor
+                }
+            }"
+        )
+
+        //MANIPULACIÓN DEL CÓDIGO DE LAMBDAS
+
+        //Declaramos un array de 10 elementos con el valor 5 en todas sus posiciones con una expresion lambda
+        var array4: IntArray = IntArray(10) { 5 }
+        println("Array4: "); array4.show();
+
+        var array5: IntArray =
+            IntArray(10) { it } //it es el parámetro que tienen las lambdas de este tipo Array para iterar
+        // y rellenar todas las posiciones de un Array con el valor que le hayamos indicado. Al estar asignando el valor del iterador,
+        // todas las posiciones del Array tendrán el valor del iterador
+        println("Array5: "); array5.show();
+
+
+        //Otro ejemplo con el que interaccionamos con el iterador para asignar un valor a las posiciones del IntArray
+        var array6: IntArray = IntArray(10) { it * 2 }
+        println("Array6: "); array6.show();
+
+        //Este ejemplo renombramos la variable del iterador, pero no podemos manipular su valor
+        var array7: IntArray = IntArray(10) { i -> i * 3 }
+        println("Array7: "); array7.show();
+
+        var suma = 0
+
+        recorrerArray(array7) {
+            suma += it
+        }
+        println("Suma del array7 : $suma")
+
+        //Ejemplo práctico de un array de tipo Char para formar una frase
+
+        var letras: Array<Char> = arrayOf('H', 'o', 'l', 'a', ' ', 'M', 'u', 'n', 'd', 'o')
+        var frase: String = ""
+
+        recorrerArray2(letras) {
+            frase += it
+        }
+        println(frase)
+
+
     }
+
+    private fun recorrerArray2(letras: Array<Char>, function: (Char) -> Unit) {
+        for (i in letras) {
+            function(i)
+        }
+    }
+
+
+    private fun recorrerArray(array: IntArray, function: (Int) -> Unit) {
+
+        for (i in array) {
+            function(i)
+        }
+    }
+
     //FUNCIONES DE EXTENSIÓN
 
     //Cogemos de un objeto existente "String" y le añadimos un método (no a la clase String) para que nos reemplace
@@ -80,5 +195,27 @@ class MainActivity : AppCompatActivity() {
     private fun multiplicar(x: Int, y: Int) = x * y
 
     private fun dividir(x: Int, y: Int) = x / y
+
+    //FUNCIONES DE ORDEN SUPERIOR EN OBJETOS
+
+
+    /*Creación de una función de extension de (Person) donde devolveremos los siguientes métodos para evaluar
+    nuestra altura, es un ejemplo para ver como se utilizan, lo ideal es mantenerlo en nuestras clases, solo operar este tipo
+    de funciones de extensión en clases ya definidas por Kotlin
+
+    fun Person.checkPolice(fn: (Float?) -> Boolean): Boolean {
+        return fn(this.height)
+    }
+
+     */
+
+    //Creación de los métodos que determinaran si podemos ser o no policías según nuestra altura y el País al que apliquemos
+    private fun alturaColombia(altura: Float?): Boolean {
+        return altura!! >= 1.60f
+    }
+
+    private fun alturaSpain(altura: Float?): Boolean {
+        return altura!! >= 1.65f
+    }
 
 }
--- app/src/main/java/com/example/kotlin_avanzado/Person.kt
+++ app/src/main/java/com/example/kotlin_avanzado/Person.kt
@@ -1,4 +1,16 @@
 package com.example.kotlin_avanzado
 
-class Person {
+class Person(var name: String? = null, var passport: String? = null, var height: Float? = null) {
+    var alive: Boolean = true
+
+
+    fun die() {
+        this.alive = false
+    }
+
+    fun checkPolice(fn: (Float?) -> Boolean): Boolean {
+        return fn(height)
+    }
+
+
 }
